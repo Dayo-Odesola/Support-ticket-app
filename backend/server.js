76f61a92c1d93209442cc035955f5004a58bdf6b
Dayo-Odesola <dayoodesola2001@gmail.com>
2022-09-10 16:38:58 +0100

    error handling and Express async handler

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const userRoutes = require('./routes/userRoutes')
+const { errorHandler } = require('./middlewares/errorMiddleware')
 
 const express = require('express')
 const dotenv = require('dotenv').config()
@@ -9,10 +10,16 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
+// middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
+
 app.get('/api/users', (req, res) => {
   res.status(200).json({ message: 'Welcome to the support Desk API'})
 })
 
+// Routes
 app.use('/api/users', userRoutes)
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
